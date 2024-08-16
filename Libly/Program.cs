57f3edee6eda7dc39cad0534ef8e29e20d8d000d
Ravi K R <raviram.mysore@gmail.com>
2024-08-16 13:05:21 +0530

    Display a welcome message on to the browser

--- Libly/Program.cs
+++ Libly/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
+//Stuff to do before you build the app, like requesting services you need
 var app = builder.Build();
-
+//Stuff to do after you build the app, like configure routes
 app.MapGet("/", () => "Welcome to Libly!");
-
+//finally run the app
 app.Run();
