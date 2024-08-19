87832d0eeab4ac6363e042991cef3125b16a991b
Ravi K R <raviram.mysore@gmail.com>
2024-08-19 21:32:57 +0530

    Modify the create and edit pages to now show a dropdown to choose the category (use a vm)

--- Libly/Pages/Books/Create.cshtml.cs
+++ Libly/Pages/Books/Create.cshtml.cs
@@ -1,23 +1,47 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Libly.Data;
 using Libly.Models;
+using System.Collections.Generic;
+using Libly.ViewModels;
 
 namespace Libly.Pages.Books
 {
     public class CreateModel : PageModel
     {
         [BindProperty]
-        public Book Book { get; set; }
+        public BookViewModel BookVM { get; set; }
+
+        public List<SelectListItem> CategoryOptions { get; set; }
+
+        public void OnGet()
+        {
+            //later we will remove this hardcoding
+            CategoryOptions = new List<SelectListItem>
+            {
+                new SelectListItem { Value = "1", Text = "Fiction" },
+                new SelectListItem { Value = "2", Text = "Science Fiction" }
+            };
+        }
 
         public IActionResult OnPost()
         {
             if (!ModelState.IsValid)
             {
+                OnGet(); // Reload dropdown options
                 return Page();
             }
 
-            BooksData.Create(Book);
+            // Map the view model to the actual Book model
+            var book = new Book
+            {
+                Title = BookVM.Title,
+                Dop = BookVM.Dop,
+                CategoryId = BookVM.CategoryId            
+            };
+
+            BooksData.Create(book);
 
             return RedirectToPage("./Index");
         }
--- Libly/Pages/Books/Edit.cshtml.cs
+++ Libly/Pages/Books/Edit.cshtml.cs
@@ -2,23 +2,43 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Libly.Data;
 using Libly.Models;
+using Libly.ViewModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Libly.Pages.Books
 {
     public class EditModel : PageModel
     {
         [BindProperty]
-        public Book Book { get; set; }
+        public BookViewModel BookVM { get; set; }
+
+        public List<SelectListItem> CategoryOptions { get; set; }
 
         public IActionResult OnGet(int id)
         {
-            // Retrieve the book to be edited
-            Book = BooksData.GetById(id);
+            // Load the book from the in-memory collection
+            var book = BooksData.GetById(id);
 
-            if (Book == null)
+            if (book == null)
             {
                 return NotFound();
             }
+
+            // Populate the view model
+            BookVM = new BookViewModel
+            {
+                Id = book.Id,
+                Title = book.Title,
+                Dop = book.Dop,
+                CategoryId = book.CategoryId
+            };
+
+            // Populate the category options
+            CategoryOptions = new List<SelectListItem>
+            {
+                new SelectListItem { Value = "1", Text = "Fiction" },
+                new SelectListItem { Value = "2", Text = "Science Fiction" }
+            };
 
             return Page();
         }
@@ -27,14 +47,31 @@ namespace Libly.Pages.Books
         {
             if (!ModelState.IsValid)
             {
-                // Return the same page with validation errors if the model state is invalid
+                // Reload the category options if validation fails
+                CategoryOptions = new List<SelectListItem>
+                {
+                    new SelectListItem { Value = "1", Text = "Fiction" },
+                    new SelectListItem { Value = "2", Text = "Science Fiction" }
+                };
                 return Page();
             }
 
-            // Update the book in the static collection
-            BooksData.Update(Book);
+            // Load the book from the in-memory collection
+            var bookToUpdate = BooksData.GetById(BookVM.Id);
+            if (bookToUpdate == null)
+            {
+                return NotFound();
+            }
 
-            // Redirect back to the Index page after successful update
+            // Update the book with the new values
+            bookToUpdate.Title = BookVM.Title;
+            bookToUpdate.Dop = BookVM.Dop;
+            bookToUpdate.CategoryId = BookVM.CategoryId;
+            
+            bookToUpdate.ModifiedOn = DateTime.Now;
+
+            BooksData.Update(bookToUpdate);
+
             return RedirectToPage("./Index");
         }
     }
