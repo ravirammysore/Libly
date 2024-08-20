36d780f0bb97f6924edc70a1da01ee48d374adb9
Ravi K R <raviram.mysore@gmail.com>
2024-08-20 15:01:10 +0530

    Modify the Delete page to delete data using EF

--- Libly/Pages/Books/Delete.cshtml.cs
+++ Libly/Pages/Books/Delete.cshtml.cs
@@ -2,18 +2,26 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Libly.Data;
 using Libly.Models;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace Libly.Pages.Books
 {
     public class DeleteModel : PageModel
     {
         [BindProperty]
-        public Book Book { get; set; }
+        public Book? Book { get; set; }
 
         public IActionResult OnGet(int id)
         {
             // Retrieve the book to be deleted
-            Book = BooksData.GetById(id);
+            //Book = BooksData.GetById(id);
+            using (var context = new BooksContext())
+            {
+                Book = context.Books
+                            .Include(b => b.Category)
+                            .SingleOrDefault(b => b.Id == id);
+            }
 
             if (Book == null)
             {
@@ -26,8 +34,16 @@ namespace Libly.Pages.Books
         public IActionResult OnPost()
         {
             // Delete the book from the static collection
-            BooksData.Delete(Book.Id);
+            //BooksData.Delete(Book.Id);
 
+            if(Book != null)
+            {
+                using (var context = new BooksContext())
+                {
+                    context.Books.Remove(Book);
+                    context.SaveChanges();
+                }
+            }           
             // Redirect back to the Index page after deletion
             return RedirectToPage("./Index");
         }
