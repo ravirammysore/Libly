fb046cc3f1854bd8ddda43d7b0169726ee16c605
Ravi K R <raviram.mysore@gmail.com>
2024-08-20 11:53:13 +0530

    Modify the index page model to fetch the data using EF

--- Libly/Pages/Books/Index.cshtml.cs
+++ Libly/Pages/Books/Index.cshtml.cs
@@ -2,15 +2,20 @@ using Libly.Data;
 using Libly.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Libly.Pages.Books
-{    
+{
     public class IndexModel : PageModel
     {
         public List<Book> Books { get; set; }
         public void OnGet()
         {
-            Books = BooksData.GetAll();
+            //Books = BooksData.GetAll();
+            using (var context = new BooksContext())
+            {
+                Books = context.Books.Include(i => i.Category).ToList();
+            }
         }
-    }   
+    }
 }
