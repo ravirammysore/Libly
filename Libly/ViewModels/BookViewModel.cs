1aae3c5a160206be1802582709f59acbb4cf2f36
Ravi K R <raviram.mysore@gmail.com>
2024-08-28 00:04:51 +0530

    Handle model validation errors in forms

--- Libly/ViewModels/BookViewModel.cs
+++ Libly/ViewModels/BookViewModel.cs
@@ -1,8 +1,12 @@
-﻿namespace Libly.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Libly.ViewModels
 {
     public class BookViewModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="We need a title for the book!")]
         public string Title { get; set; }
         public DateTime Dop { get; set; }
         public int CategoryId { get; set; }
