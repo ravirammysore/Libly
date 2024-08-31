b4f7d43c8ad8fe0ea93c8455be6b6d31d1d63c8f
Ravi K R <raviram.mysore@gmail.com>
2024-08-31 20:13:02 +0530

    Use inline data for multiple writing multiple tests for the same behaviour

--- Libly.Test/BookTests.cs
+++ Libly.Test/BookTests.cs
@@ -4,17 +4,23 @@ namespace Libly.Test
 {
     public class BookTests
     {
-        [Fact]       
-        public void GetFormattedTitle_ReturnsCorrectlyFormattedTitle()
+        [Theory]
+        [InlineData("the great gatsby", "The Great Gatsby")]
+        [InlineData("TO KILL A MOCKINGBIRD", "To Kill a Mockingbird")]
+        [InlineData("the catcher in the rye", "The Catcher in the Rye")]
+        [InlineData("a tale of two cities", "A Tale of Two Cities")]
+        [InlineData("", "")]
+        [InlineData("the", "The")]
+        public void GetFormattedTitle_ReturnsCorrectlyFormattedTitle(string input, string expected)
         {
             // Arrange
-            var book = new Book { Title = "the great gatsby" };
+            var book = new Book { Title = input };
 
             // Act
             var result = book.GetFormattedTitle();
 
             // Assert
-            Assert.Equal("The Great Gatsby", result);
+            Assert.Equal(expected, result);
         }
     }
 }
