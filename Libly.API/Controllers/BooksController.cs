a02f150f0519b08f6e9ae49d98c3f64e28fde93a
Ravi K R <raviram.mysore@gmail.com>
2024-09-02 06:19:31 +0530

    Now do the same using an MVC style controller

---
+++ Libly.API/Controllers/BooksController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Libly.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> Get()
+        {
+            var books = new List<Book>
+            {
+                new Book { Id = 1, Title = "The Great Gatsby", Dop = new DateTime(1925, 4, 10) },
+                new Book { Id = 2, Title = "To Kill a Mockingbird", Dop = new DateTime(1960, 7, 11) },
+                new Book { Id = 3, Title = "1984", Dop = new DateTime(1949, 6, 8) }
+            };
+            return books;
+        }
+    }
+
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Dop { get; set; }
+    }
+}
--- Libly.API/Program.cs
+++ Libly.API/Program.cs
@@ -1,21 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
 
-app.MapGet("/books", () =>
-{
-    var books = new List<Book>
-    {
-        new Book { Id = 1, Title = "The Great Gatsby", Dop = new DateTime(1925, 4, 10) },
-        new Book { Id = 2, Title = "To Kill a Mockingbird", Dop = new DateTime(1960, 7, 11) },
-        new Book { Id = 3, Title = "1984", Dop = new DateTime(1949, 6, 8) }
-    };
-    return books;
-});
+app.MapControllers();
 
 app.Run();
 
