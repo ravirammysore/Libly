c77eb957ceed994f5bc64f63e3e96b6fee9a6902
Ravi K R <raviram.mysore@gmail.com>
2024-09-02 05:57:39 +0530

    Create a new ASP.NET Core Web API project named Libly.API - for now return an in-memory book list

---
+++ Libly.API/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
+
+app.MapGet("/books", () =>
+{
+    var books = new List<Book>
+    {
+        new Book { Id = 1, Title = "The Great Gatsby", Dop = new DateTime(1925, 4, 10) },
+        new Book { Id = 2, Title = "To Kill a Mockingbird", Dop = new DateTime(1960, 7, 11) },
+        new Book { Id = 3, Title = "1984", Dop = new DateTime(1949, 6, 8) }
+    };
+    return books;
+});
+
+app.Run();
+
+class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public DateTime Dop { get; set; }
+}
