dfe5b767bdf5faf0c17c904fd26889c48c6b2e0e
Ravi K R <raviram.mysore@gmail.com>
2024-09-02 06:47:09 +0530

    Modify the BooksController to get data from database using context via DI and test run the API

--- Libly.API/Controllers/BooksController.cs
+++ Libly.API/Controllers/BooksController.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Libly.Core.Data;
+using Libly.Core.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Libly.API.Controllers
 {
@@ -6,6 +11,17 @@ namespace Libly.API.Controllers
     [Route("api/[controller]")]
     public class BooksController : ControllerBase
     {
-        
-    }   
+        private readonly BooksContext _context;
+
+        public BooksController(BooksContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> GetBooks()
+        {
+            return _context.Books.ToList();
+        }
+    }
 }
