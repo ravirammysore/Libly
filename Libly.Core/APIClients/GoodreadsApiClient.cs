4317f7233b40f4d36120137bfaf7f6116364fd16
Ravi K R <raviram.mysore@gmail.com>
2024-09-22 12:41:46 +0530

    Added simple comment and pushing to repo

--- Libly.Core/APIClients/GoodreadsApiClient.cs
+++ Libly.Core/APIClients/GoodreadsApiClient.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Libly.Core.APIClients
 {
+    //A unit-test for book must NOT be using and instance of this
     public class GoodreadsApiClient : IGoodreadsApiClient
     {
         public double GetBookRating(string bookTitle)
